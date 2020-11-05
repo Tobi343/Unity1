1f97bdfcaaacf1ad5731c5edf15022047623399f
SebastianStecher <47062722+SebastianStecher@users.noreply.github.com>
2020-11-05 10:05:30 +0100

    .

---
+++ Game/PacMan/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coin;
+    public bool horizontalSpawner;
+    public bool verticalSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (horizontalSpawner)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
